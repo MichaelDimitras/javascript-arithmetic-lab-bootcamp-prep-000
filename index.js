f8d531b0d67465b7b4abdec3f5002964115da3fc
Michael Dimitras <michaeladimitras@gmail.com>
2017-09-05 23:09:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ function multiply(a,b){
 function divide(a,b){
   return a/ b;
 }
+
+
+function inc(a){
+  return a += 1;
+}
+
+function dec(a){
+  return a =- 1;
+}
+
+
+function makeInt(a){
+  return parseIne(a,10);
+}
+
